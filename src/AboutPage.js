e6a7f22625cf202924ba648428b99901ee40aa85
Jasper Levy <jglevy@princeton.edu>
2023-12-25 14:38:13 -0500

    about page completed

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import headShot from './images/headshot.png'
+import fencing from './images/fencing.jpg'
 
 function AboutPage() {
   return (
 <div className="about-div">
         <div className="text-column">
-          <p>I am a senior studying computer science at Princeton University. I hold a certificate (minor) in Statistics and Machine Learning. I have an expected graduation date of May 2024. </p>
-          <p>I have experience in software development, academic research, modern machine learning techniques, and leadership through professional and volunteer work. Some more specific interests of mine include GIS applications, decentralized systems, technology policy, and healthcare innovation.</p>
-          <p>I love geography and hiking - so much so that I started a new project. I am currently working on TrekAI, a budget-tailored hiking recommendation platform.</p>
+          <p>I spend a lot of my time fencing. I am a foil fencer <a href ="https://goprincetontigers.com/sports/mens-fencing" target ="blank">Princeton's Varsity Team</a> - one of the best performing D1 programs in the country. </p>
+          <p>I also love surfing, traveling, architecture history, and discovering new music.</p>
+          <p>For resume access, please send an email to jglevy@princeton.edu or DM me on LinkedIn.</p>
         </div>
         <div className="image-column">
-          <img src={headShot} alt="Description" width="200" height="auto" />
+          <img src={fencing} alt="Description" width="200" height="auto" />
         </div>
       </div>
   );
